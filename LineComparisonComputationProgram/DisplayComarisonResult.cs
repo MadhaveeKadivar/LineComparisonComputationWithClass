7f7ea96e253b5216d873568051c874008f44034b
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-03-23 18:11:20 +0530

    [Madhavee] Adding DisplayComparisonResult.cs

---
+++ LineComparisonComputationProgram/DisplayComarisonResult.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonComputationProgram
+{
+    internal class DisplayComarisonResult
+    {
+        double lineLength1, lineLength2;
+        public void FirstLineLength()
+        {
+            double x1, y1, x2, y2;
+
+            Console.WriteLine("\nEnter first point X coordinate x1: ");
+            x1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter first point Y coordinate y1: ");
+            y1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nEnter second point X coordinate x2: ");
+            x2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter second point Y coordinate y2: ");
+            y2 = Convert.ToDouble(Console.ReadLine());
+
+            double powerx = Math.Pow((x2 - x1), 2);
+            double powery = Math.Pow((y2 - y1), 2);
+
+            lineLength1 = Math.Sqrt((powerx + powery));
+
+            Console.WriteLine($"\nLength of line using two points ({x1},{y1}) and ({x2},{y2}) is : {lineLength1}");
+        }
+
+        public void SecondLineLength()
+        {
+            double x1, y1, x2, y2;
+
+            Console.WriteLine("\nEnter first point X coordinate x1: ");
+            x1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter first point Y coordinate y1: ");
+            y1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nEnter second point X coordinate x2: ");
+            x2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter second point Y coordinate y2: ");
+            y2 = Convert.ToDouble(Console.ReadLine());
+
+            double powerx = Math.Pow((x2 - x1), 2);
+            double powery = Math.Pow((y2 - y1), 2);
+
+            lineLength2 = Math.Sqrt((powerx + powery));
+
+            Console.WriteLine($"\nLength of line using two points ({x1},{y1}) and ({x2},{y2}) is : {lineLength2}");
+        }
+
+        public void DisplayResult()
+        {
+            int result = lineLength1.CompareTo(lineLength2);
+
+            Console.WriteLine($"\n\nLength of First Line: {lineLength1}");
+            Console.WriteLine($"Length of Second Line: {lineLength2}");
+
+            if (result > 0)
+                Console.WriteLine($"\n So First line is greater than Second line");
+
+            else if (result < 0)
+                Console.WriteLine($"\n So First line is less than Second line");
+
+            else
+                Console.WriteLine($"\nSo First Line and Second Line Both are Equal");
+                                       
+        }
+    }
+}
--- LineComparisonComputationProgram/Program.cs
+++ LineComparisonComputationProgram/Program.cs
@@ -11,17 +11,23 @@ namespace LineComparisonComputationProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Computation Program");
+            //UC1
+            //FindLineLength findLineLength = new FindLineLength();
+            //findLineLength.Length();
 
-           // FindLineLength findLineLength = new FindLineLength();
-           // findLineLength.Length();
+            //UC2
+            //TwoLineComparison twoLineComparison = new TwoLineComparison();
+            //Console.WriteLine("\nFor First Line......");
+            //twoLineComparison.FirstLineLength();
+            //Console.WriteLine("\n\nFor Second Line......");
+            //twoLineComparison.SecondLineLength();
+            //Console.WriteLine("\n\nComparison between lines....");
+            //twoLineComparison.Comparison();
 
-            TwoLineComparison twoLineComparison = new TwoLineComparison();
-            Console.WriteLine("\nFor First Line......");
-            twoLineComparison.FirstLineLength();
-            Console.WriteLine("\n\nFor Second Line......");
-            twoLineComparison.SecondLineLength();
-            Console.WriteLine("\n\nComparison between lines....");
-            twoLineComparison.Comparison();
+            DisplayComarisonResult displayComarisonResult = new DisplayComarisonResult();
+            displayComarisonResult.FirstLineLength();
+            displayComarisonResult.SecondLineLength();
+            displayComarisonResult.DisplayResult();
 
             Console.ReadLine();
         }
