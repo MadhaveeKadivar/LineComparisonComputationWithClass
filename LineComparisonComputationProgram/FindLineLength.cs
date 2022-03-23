0ada02f596f370d979dac3188ae6977a7f58a544
MadhaveeKadivar <100771918+MadhaveeKadivar@users.noreply.github.com>
2022-03-23 17:05:21 +0530

    Update FindLineLength.cs

--- LineComparisonComputationProgram/FindLineLength.cs
+++ LineComparisonComputationProgram/FindLineLength.cs
@@ -18,10 +18,10 @@ namespace LineComparisonComputationProgram
             Console.WriteLine("Enter first point Y coordinate y1: ");
             y1 = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Enter first point X coordinate x2: ");
+            Console.WriteLine("Enter second point X coordinate x2: ");
             x2 = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Enter first point Y coordinate y2: ");
+            Console.WriteLine("Enter second point Y coordinate y2: ");
             y2 = Convert.ToDouble(Console.ReadLine());
 
             double powerx = Math.Pow((x2 - x1), 2);
