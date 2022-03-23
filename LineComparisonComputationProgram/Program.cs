99e48cd319fbfa11ab87c779bf63cb359fdd1de2
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-03-23 17:46:21 +0530

    [Madhavee] Adding TwoLineComparison.cs

--- LineComparisonComputationProgram/Program.cs
+++ LineComparisonComputationProgram/Program.cs
@@ -12,8 +12,16 @@ namespace LineComparisonComputationProgram
         {
             Console.WriteLine("Welcome to Line Comparison Computation Program");
 
-            FindLineLength findLineLength = new FindLineLength();
-            findLineLength.Length();
+           // FindLineLength findLineLength = new FindLineLength();
+           // findLineLength.Length();
+
+            TwoLineComparison twoLineComparison = new TwoLineComparison();
+            Console.WriteLine("\nFor First Line......");
+            twoLineComparison.FirstLineLength();
+            Console.WriteLine("\n\nFor Second Line......");
+            twoLineComparison.SecondLineLength();
+            Console.WriteLine("\n\nComparison between lines....");
+            twoLineComparison.Comparison();
 
             Console.ReadLine();
         }
---
+++ LineComparisonComputationProgram/TwoLineComparison.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonComputationProgram
+{
+    internal class TwoLineComparison
+    {
+        double lineLength1, lineLength2;
+        public void FirstLineLength()
+        {
+            double x1, y1, x2, y2;
+
+            Console.WriteLine("\nEnter first point X coordinate x1: ");
+            x1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter first point Y coordinate y1: ");
+            y1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nEnter second point X coordinate x2: ");
+            x2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter second point Y coordinate y2: ");
+            y2 = Convert.ToDouble(Console.ReadLine());
+
+            double powerx = Math.Pow((x2 - x1), 2);
+            double powery = Math.Pow((y2 - y1), 2);
+
+            lineLength1 = Math.Sqrt((powerx + powery));
+
+            Console.WriteLine($"\nLength of line using two points ({x1},{y1}) and ({x2},{y2}) is : {lineLength1}");
+        }
+
+        public void SecondLineLength()
+        {
+            double x1, y1, x2, y2;
+
+            Console.WriteLine("\nEnter first point X coordinate x1: ");
+            x1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter first point Y coordinate y1: ");
+            y1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nEnter second point X coordinate x2: ");
+            x2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter second point Y coordinate y2: ");
+            y2 = Convert.ToDouble(Console.ReadLine());
+
+            double powerx = Math.Pow((x2 - x1), 2);
+            double powery = Math.Pow((y2 - y1), 2);
+
+            lineLength2 = Math.Sqrt((powerx + powery));
+
+            Console.WriteLine($"\nLength of line using two points ({x1},{y1}) and ({x2},{y2}) is : {lineLength2}");
+        }
+        public void Comparison()
+        {
+            Console.Write("\n\nFirst Line is Equal To Second Line : ");
+            Console.WriteLine(lineLength1.Equals(lineLength2));
+        }
+    }
+}
